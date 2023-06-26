3992c0abb9727c3d85c430cdb354fb71e5fb99e6
yback1223 <yoyob1223@gmail.com>
2023-06-26 04:43:16 -0400

    Fix user and group

--- srcs/requirements/wordpress/tools/yback_prepare_wp.sh
+++ srcs/requirements/wordpress/tools/yback_prepare_wp.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+
 sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "/etc/php/7.4/fpm/pool.d/www.conf";
-chown -R nginx:nginx /var/www/*;
+chown -R www-data:www-data /var/www/*;
 chown -R 755 /var/www/*;
 mkdir -p /run/php/;
 touch /run/php/php7.4-fpm.pid;
+
