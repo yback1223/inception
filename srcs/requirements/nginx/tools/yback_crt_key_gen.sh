532b8118867c1317af2bdd86bfe772e8b629242b
yback1223 <yoyob1223@gmail.com>
2023-06-29 17:16:22 -0400

    Add some echo cmds

--- srcs/requirements/nginx/tools/yback_crt_key_gen.sh
+++ srcs/requirements/nginx/tools/yback_crt_key_gen.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Creating Certificate and Key !!!"
+
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=KR/ST=Seoul/L=Seoul/O=${WP_TITLE}/CN=${WP_URL}";
 fi
+
+echo "Created Certificate and Key !!!"
+
+echo "Activate Nginx at foreground !!!"
 
 nginx -g 'daemon off;'
