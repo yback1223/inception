2108f2984b996e40ad27740ad0dfffaa3622231c
yback1223 <yoyob1223@gmail.com>
2023-06-29 17:16:48 -0400

    Make movement in the container

--- srcs/requirements/nginx/tools/yback_to_the_sun.sh
+++ srcs/requirements/nginx/tools/yback_to_the_sun.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+echo "Move to /var/www !!!"
+
 cd /var/www
+
+echo "Permission 777 open for yback_crt_key_gen.sh !!!"
+
 chmod 777 yback_crt_key_gen.sh
+
+echo "Execute yback_crt_key_gen.sh !!!"
 
 ./yback_crt_key_gen.sh
 
+echo "Print error logs !!!"
+
 cd /var/log/nginx
 cat error.log
+
+echo "error.log done !!!"
