9542e493dd8a88ae53401c3ce821cd3189173a3d
yback1223 <yoyob1223@gmail.com>
2023-06-29 17:20:40 -0400

    Add directory init bash file

--- srcs/requirements/yback_mkdir.sh
+++ srcs/requirements/yback_mkdir.sh
@@ -6,9 +6,11 @@ mariadb_path="${volume_path}/mariadb"
 
 
 if [ ! -d "${wordpress_path}" ]; then
+    echo "Make ${wordpress_path} directory !!!"
     mkdir -p "${wordpress_path}"
 fi
 
 if [ ! -d "${mariadb_path}" ]; then
+    echo "Make ${mariadb_path} directory !!!"
     mkdir -p "${mariadb_path}"
 fi
