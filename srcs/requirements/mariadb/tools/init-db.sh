262b700284aa883b413be449357c3959111975e7
yback1223 <yoyob1223@gmail.com>
2024-01-04 22:16:40 -0500

    Real End

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 set -e
 
-if [ ! -d "/var/lib/mysql/$MYSQL_DB" ]; then
+echo $DB_ROOT
+echo $DB_NAME
+echo $DB_USER
+echo $DB_PASS
+
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
   mysql_install_db --datadir=/var/lib/mysql --auth-root-authentication-method=normal >/dev/null
   mysqld --bootstrap << EOF
-mysql -u root -e "FLUSH PRIVILEGES; 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT'; 
-FLUSH PRIVILEGES;
-CREATE DATABASE IF NOT EXISTS ${DB_NAME};
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;"
+USE mysql;
 
-echo "MySQL initialization done!"
+FLUSH PRIVILEGES;
+
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT'; 
+
+FLUSH PRIVILEGES;
+
 EOF
 fi
 
--- srcs/requirements/wordpress/tools/init-wp.sh
+++ srcs/requirements/wordpress/tools/init-wp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# set -e
+set -e
 
 echo "      Connecting to db !!"
 while ! mysqladmin -h$DB_HOST -u$DB_USER -p$DB_PASS ping >/dev/null 2>&1; do
@@ -9,21 +9,12 @@ while ! mysqladmin -h$DB_HOST -u$DB_USER -p$DB_PASS ping >/dev/null 2>&1; do
 done
 echo "      Connected to db ~"
 
-echo "$DB_NAME"
-echo "$WP_TITLE"
-echo "$WP_USER"
 
 wp-cli core download --allow-root --force
-
-echo "11111"
-
 wp-cli config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --force
-
-echo "22222"
-
 wp-cli core install --url=$WP_URL/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
 wp-cli user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=subscriber --allow-root
+
 wp-cli theme install astra --allow-root
 wp-cli theme update astra --allow-root
 wp-cli theme activate astra --allow-root
